c0093f3daac02e0fde128ea2ca1c12272fd6df57
boudes <boudes@lipn.univ-paris13.fr>
2015-10-02 19:00:42 +0200

    un exemple pour tester

---
+++ examples_jobs/clock2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 18:51:53 2015
+
+@author: personne
+"""
+import time
+
+while True:
+    time.sleep(2)
+    print(time.clock())
